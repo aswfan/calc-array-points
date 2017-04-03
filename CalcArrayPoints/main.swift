bdffc92963f5b94926ff15c59b48bd6da01656c5
yyfan <yyfan1994@gmail.com>
2017-04-03 16:03:10 -0700

    first commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by iGuest on 4/3/17.
+//  Copyright © 2017 com.imyyfan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
