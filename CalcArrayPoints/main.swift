7b3c322f3b824052ef6ab12ee55b940d66a231c6
yyfan <yyfan1994@gmail.com>
2017-04-03 16:58:45 -0700

    second commit

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -8,5 +8,56 @@
 
 import Foundation
 
-print("Hello, World!")
+extension String
+{
+    func trim() -> String
+    {
+        return self.trimmingCharacters(in: NSCharacterSet.whitespaces)
+    }
+}
+
+func add(a: Int, b: Int) -> (Bool, Int) {
+    return (true, a + b)
+}
+
+func sub(a: Int, b: Int) -> (Bool, Int) {
+    return (true, a - b)
+}
+
+func mul(a: Int, b: Int) -> (Bool, Int) {
+    return (true, a * b)
+}
+
+func div(a: Int, b: Int) -> (Bool, Int) {
+    if b == 0 {
+        print("Illegal input: Int division by zero")
+        return (false, 0)
+    }
+    return (true, a / b)
+}
+
+func basic(a: Int, b: Int, c: String) -> Int {
+    var result = 0
+    var bool = true
+    
+    if c == "-" {
+        result = sub(a: a, b: b).1
+    }else if c == "+" {
+        result = add(a: a, b: b).1
+    }else if c == "*" {
+        result = mul(a: a, b: b).1
+    }else if c == "/" {
+        (bool, result) = div(a: a, b: b)
+        if !bool {
+            
+        }
+    }
+    return result
+}
+
+var s = "1"
+var t: Int = Int(s)
+
+
+
 
